40bf85dd80d901c26ca609046bcd9989fd8bf9a1
Arjun <arjun81815@gmail.com>
2016-08-13 21:32:58 -0700

    new

--- src/com/example/customtextimage/MainActivity.java
+++ src/com/example/customtextimage/MainActivity.java
@@ -55,8 +55,7 @@ public class MainActivity extends Activity {
 		     paint.setAntiAlias(true);
 		     paint.setTextSize(50);
 		     cv.drawText(text, 500, 500, paint);
-			 paint.setTextSize(34);
-		       FileOutputStream stream = new FileOutputStream(imagePath); //create your FileOutputStream here
+			   FileOutputStream stream = new FileOutputStream(imagePath); //create your FileOutputStream here
 			    b.compress(CompressFormat.JPEG, 85, stream);
 			    b.recycle();
 			    
@@ -81,22 +80,5 @@ public class MainActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    
 }
